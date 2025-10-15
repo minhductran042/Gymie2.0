14af70d33f13353d7ac7c52b2604301e3c54aabd
minhductran042@gmail.com <minhductran042@gmail.com>
2025-10-15 11:45:09 +0700

    feat: Tạo config cho file env

---
+++ server/src/shared/config.ts
@@ -0,0 +1,50 @@
+import z from 'zod';
+import fs from 'fs';
+import path from 'path';
+import { config } from 'dotenv';
+
+config({
+    path: '.env',
+})
+
+if(!fs.existsSync(path.resolve('.env'))) {
+    console.log('Environment file not found.')
+    process.exit(1)
+}
+
+const configSchema = z.object({
+    DATABASE_URL: z.string(),
+    APP_NAME: z.string(),
+    ACCESS_TOKEN_SECRET: z.string(),
+    ACCESS_TOKEN_EXPIRES_IN: z.string(),
+    REFRESH_TOKEN_SECRET: z.string(),
+    REFRESH_TOKEN_EXPIRES_IN: z.string(),
+    SECRET_API_KEY: z.string(),
+    ADMIN_NAME: z.string(),
+    ADMIN_PASSWORD: z.string(),
+    ADMIN_EMAIL: z.string(),
+    ADMIN_PHONENUMBER: z.string(),
+    OTP_EXPIRES_IN: z.string(),
+    RESEND_API_KEY: z.string(),
+    GOOGLE_CLIENT_ID: z.string(),
+    GOOGLE_CLIENT_SECRET: z.string(),
+    GOOGLE_REDIRECT_URI: z.string(),
+    GOOGLE_CLIENT_REDIRECT_URI: z.string(),
+    PREFIX_STATIC_ENDPOINT: z.string(),
+    S3_REGION: z.string(),
+    S3_ACCESS_KEY: z.string(),
+    S3_SECRET_KEY: z.string(),
+    S3_BUCKET_NAME: z.string()
+})
+
+const configServer = configSchema.safeParse(process.env)
+
+if(!configServer.success) {
+    console.log('Invalid environment variables:', configServer.error.format());
+    console.error(configServer.error);
+    process.exit(1);
+}
+
+const envConfig = configServer.data
+
+export default envConfig
